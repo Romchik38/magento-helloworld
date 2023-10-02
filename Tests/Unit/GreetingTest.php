8c6c62c4212e53abfa33c94accc822d3af9ac820
Romchik38 <pomahehko.c@gmail.com>
2023-10-02 18:51:47 +0300

    test via phpunit, composer.json init, readme

--- Tests/Unit/GreetingTest.php
+++ Tests/Unit/GreetingTest.php
@@ -5,27 +5,19 @@ declare(strict_types=1);
 require __DIR__ . '/../../Model/Greeting.php';
 
 use Romchik38\HelloWorld\Model\Greeting;
+use PHPUnit\Framework\TestCase;
 
-$testPhrase = 'hello world';
-
-$g1 = new Greeting($testPhrase);
-
-$message = '';
-
-// 1 checking function get
-$message = $g1->get();
-
-// 2 checking type of returning value
-$messageType = gettype($message);
-if ($messageType !== 'string') {
-    $errorMessage = "expecting type \"string\"" . $messageType . " given";
-    throw new \Exception($errorMessage);
+class GreetingTest extends TestCase
+{
+    public function testGet(): void
+    {
+        $testPhrase = 'hello world';
+        $g1 = new Greeting($testPhrase);
+        $message = '';
+        $message = $g1->get();
+        // checking type of returning value
+        $messageType = gettype($message);
+        $this->assertSame('string', $messageType);
+        $this->assertSame($testPhrase, $message);
+    }
 }
-
-// 3 checking message value
-if ($message !== $testPhrase) {
-    $errorMessage = "expecting message: \"" . $testPhrase . "\", given: \"" . $message . "\"";
-    throw new \Exception($errorMessage);
-}
-
-echo 'test passed: ' . Greeting::class;
--- readme.md
+++ readme.md
@@ -1,1 +1,7 @@
-print 'hello world'
+# Simple magento 2 module  
+
+## Description  
+Prints 'hello world'  
+
+## Test
+Run `phpunit Tests/`  
